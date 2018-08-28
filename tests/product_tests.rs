acd7cad9d8d04a92fafc747648d8a4fef16536fe
werner <werner@yahoo.es>
2018-08-28 16:55:10 -0300

    add product_test file for testing products handlers

---
+++ tests/product_tests.rs
@@ -0,0 +1,24 @@
+extern crate rocket;
+extern crate regex;
+
+use regex::Regex;
+use rocket::http::ContentType;
+use rocket::http::Status;
+use rocket::local::Client;
+
+pub fn index(client: Client) {
+    let mut response = client
+        .post("/products")
+        .header(ContentType::JSON)
+        .body(r#"{
+            "name": "Shoe",
+            "description": "for the feet"
+        }"#)
+        .dispatch();
+    assert_eq!(response.status(), Status::Ok);
+
+    response = client.get("/products").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let re = Regex::new(r#","name":"Shoe","description":"for the feet","stock":0.0},null"#).unwrap();
+    assert!(re.is_match(&response.body_string().unwrap()));
+}
