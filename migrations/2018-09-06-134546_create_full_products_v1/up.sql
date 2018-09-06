2149b632ddf65e5ff74aea16ccb64f975b7a6617
werner <werner@yahoo.es>
2018-09-06 10:50:42 -0300

    Add full_products migration

---
+++ migrations/2018-09-06-134546_create_full_products_v1/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+
+CREATE VIEW full_products AS
+SELECT p.id, p.name, p.description, p.stock,
+       json_agg(distinct pc.*) AS product_costs,
+       json_agg(distinct pp.*) AS price_costs
+       FROM products p
+       LEFT JOIN (SELECT pc.*, c.name FROM product_costs AS pc INNER JOIN costs c ON pc.cost_id = c.id) AS pc ON p.id = pc.product_id
+       LEFT JOIN (SELECT pp.*, p.name FROM product_prices AS pp INNER JOIN prices p ON pp.price_id = p.id) AS pp ON p.id = pp.product_id
+       GROUP BY p.id;
